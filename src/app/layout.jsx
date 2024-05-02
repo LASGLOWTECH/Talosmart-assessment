7d0e250d9828e18c773915715537cd9ca47ba839
Omozemoje Augustine Oisasoje <106359138+LASGLOWTECH@users.noreply.github.com>
2024-05-02 01:23:18 -0700

    made change

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,28 +1,27 @@
 import './globals.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import { Rubik } from 'next/font/google'
-import Link from "next/link";
+// import Footer from './components/Footer'
+// import Navbar from './components/Navbar'
+import { Inter } from 'next/font/google'
 
 
-const rubik = Rubik({ subsets: ['latin'] })
+const inter= Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'my folio-app',
+  title: 'procuregrids ',
   description: 'A website for my ',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={rubik.className}
+      <body className={inter.className}
       
       >
-      <Navbar/>
+     
         
         
         {children}
-        <Footer/>
+       
         
         </body>
     </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,32 @@
-
+import CountdownTimer from "./components/CountdownTimer";
 import Link from "next/link";
-
+import Image from "next/image";
 export default function Home() {
-  return (
-    <main className="min-h-screen p-24   flex items-center justify-center flex-col bg-gradient-to-r from-darkBlue to-darkestBlue">
 
-      <section className=''>
-      <h1 className='text-white text-3xl text-center font-extrabold'>TALOSMART ASSESSMENT</h1>
-      <p className='text-lightGreen pt-2 text-center text-base font-light'>This a short next js  project for a user Sign in and Register  </p>    
-  
-  <div className="submit flex  flex-col justify-start ">
-          
-        
-          <div  className="text-lg text-white flex place-self-start  focus:outline-none rounded-md px-16 py-2  my-3 bg-darkGreen">
-<Link href="/register">Register</Link>
+  // Set your target date (e.g., '2024-05-10T00:00:00Z' for May 10, 2024)
+  const targetDate = '2024-08-10T00:00:00Z';
+
+  return (
+    <main className="min-h-screen p-16  flex items-center justify-center flex-col bg-gradient-to-r from-Armygreen to-darkBlue">
+      <Link href="/#">  <Image className="pb-6 size-auto"
+        src="/pgrid.svg"
+        alt="procuregrids"
+        width={200}
+        height={150}></Image></Link>
+      <section className=' flex flex-col items-center justify-center'>
+      
+
+        <h1 className='text-white text-3xl  md:text-7xl py-6 text-center  shadow-2xl px-3 shadow-Lemongreen font-extrabold'>We are coming soon</h1>
+        <p className='text-gray-300 pt-2 text-center text-base md:max-w-2xl pb-6 font-light'> Procure Grids is Africa's first construction cost control technology. We host a market place that connects construction material vendors/supplier to construction companies, real estate developers, government entitiesand independent private builders. 
+Welcome to our wait-list.
+We go live in 3 months. Get ready to witness something extraordinary! Our launch is just around the corner. Stay tuned for an experience like never before  </p>
+       
+        <CountdownTimer targetDate={targetDate} />
+        <div className="submit flex  flex-col justify-start  pt-5">
+
+
+          <div className="text-lg text-white flex place-self-start    hover:bg-white hover:text-Armygreen focus:outline-none rounded-md px-6 py-3 shadow-Armygreen  my-3 bg-Lemongreen">
+            <Link href="https://chat.whatsapp.com/HsmSDaMWczZAer8iWtcS1x">Learn More</Link>
 
           </div>
 
@@ -22,8 +35,8 @@ export default function Home() {
 
 
       </section>
-      
-      </main>
-  
+
+    </main>
+
   )
 }
